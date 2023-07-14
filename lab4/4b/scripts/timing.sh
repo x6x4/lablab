58d9f0c470da2a8ba15b9a2e6504a5352c1fcc0f
Kseniya <kseniya.d.bakshaeva@openelbrus.ru>
2023-07-14 22:45:50 +0300

    4b; refactor: rewrite timing.sh and Makefile, change folder structure

--- lab4/4b/scripts/timing.sh
+++ lab4/4b/scripts/timing.sh
@@ -4,8 +4,7 @@
 #  data preprocessing 
 preproc() 
 {
-    ../timing.bin > ../test 
-    rm ord_del ord_ins ord_search ord_trav
+    ./timing.bin > build/test 
     EPOCHS=20
 }
 
@@ -13,27 +12,33 @@ preproc()
 #  select the lines corresponding to each operation
 selection() 
 {
-    awk 'NR%3==1' ../test | awk '{print $5}' > search
-    awk 'NR%3==2' ../test | awk '{print $5}' > insertion
-    awk 'NR%3==0' ../test | awk '{print $5}' > deletion
-    #awk 'NR%4==0' ../test | awk '{print $5}' > traversal
+    awk 'NR%3==1' ../test | awk '{print $5}' > build/search
+    awk 'NR%3==2' ../test | awk '{print $5}' > build/insertion
+    awk 'NR%3==0' ../test | awk '{print $5}' > build/deletion
+    #awk 'NR%4==0' ../test | awk '{print $5}' > build/traversal
 }
 
 ############################################
 # sort lines by the number of nodes in tests
 sort ()
 {
+    #  successfully fill files with nothing
+    : > build/ord_search
+    : > build/ord_ins
+    : > build/ord_del
+    : > build/ord_trav
+
     for i in $(seq 1 $((EPOCHS - 1)))
     do 
-        awk 'NR%'$EPOCHS'=='${i} search >> ord_search
-        awk 'NR%'$EPOCHS'=='${i} insertion >> ord_ins
-        awk 'NR%'$EPOCHS'=='${i} deletion >> ord_del
-        #awk 'NR%'$EPOCHS'=='${i} traversal >> ord_trav
+        awk 'NR%'$EPOCHS'=='${i} build/search >> build/ord_search
+        awk 'NR%'$EPOCHS'=='${i} build/insertion >> build/ord_ins
+        awk 'NR%'$EPOCHS'=='${i} build/deletion >> build/ord_del
+        #awk 'NR%'$EPOCHS'=='${i} build/traversal >> build/ord_trav
     done 
-        awk 'NR%'$EPOCHS'==0' search >> ord_search
-        awk 'NR%'$EPOCHS'==0' insertion >> ord_ins
-        awk 'NR%'$EPOCHS'==0' deletion >> ord_del
-        #awk 'NR%'$EPOCHS'==0' traversal >> ord_trav
+        awk 'NR%'$EPOCHS'==0' build/search >> build/ord_search
+        awk 'NR%'$EPOCHS'==0' build/insertion >> build/ord_ins
+        awk 'NR%'$EPOCHS'==0' build/deletion >> build/ord_del
+        #awk 'NR%'$EPOCHS'==0' build/traversal >> build/ord_trav
 }
 
 ############################################
@@ -52,7 +57,7 @@ medium_search()
     for i in $(seq 1 $((EPOCHS)))
     do
         sum_s=0
-        sed -n "$(($EPOCHS*i-$EPOCHS + 1)), $(($EPOCHS*i))p" ord_search | awk '{sum_s += $1} END {x=sum_s/'$EPOCHS'*100; print x}'
+        sed -n "$(($EPOCHS*i-$EPOCHS + 1)), $(($EPOCHS*i))p" build/ord_search | awk '{sum_s += $1} END {x=sum_s/'$EPOCHS'*100; print x}'
     done
 }
 ############################################
@@ -62,7 +67,7 @@ medium_ins()
     for i in $(seq 1 $((EPOCHS)))
     do
         sum_i=0
-        sed -n "$(($EPOCHS*i-$EPOCHS + 1)), $(($EPOCHS*i))p" ord_ins | awk '{sum_i += $1} END {x=sum_i/'$EPOCHS'*100; print x}'
+        sed -n "$(($EPOCHS*i-$EPOCHS + 1)), $(($EPOCHS*i))p" build/ord_ins | awk '{sum_i += $1} END {x=sum_i/'$EPOCHS'*100; print x}'
     done
 }
 ############################################
@@ -72,7 +77,7 @@ medium_del()
     for i in $(seq 1 $((EPOCHS)))
     do
         sum_d=0
-        sed -n "$(($EPOCHS*i-$EPOCHS + 1)), $(($EPOCHS*i))p" ord_del | awk '{sum_d += $1} END {x=sum_d/'$EPOCHS'*100; print x}'
+        sed -n "$(($EPOCHS*i-$EPOCHS + 1)), $(($EPOCHS*i))p" build/ord_del | awk '{sum_d += $1} END {x=sum_d/'$EPOCHS'*100; print x}'
     done
 }
 ############################################
@@ -82,7 +87,7 @@ medium_trav()
     for i in $(seq 1 $((EPOCHS)))
     do
         sum_t=0
-        sed -n "$(($EPOCHS*i-$EPOCHS + 1)), $(($EPOCHS*i))p" ord_trav | awk '{sum_t += $1} END {x=sum_t/'$EPOCHS'; print x}'
+        sed -n "$(($EPOCHS*i-$EPOCHS + 1)), $(($EPOCHS*i))p" build/ord_trav | awk '{sum_t += $1} END {x=sum_t/'$EPOCHS'; print x}'
     done
 }
 
